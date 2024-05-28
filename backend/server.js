6d1fe76f2772172c2405314aeb891a3f66d78318
Harsh bindal <108680668+Harsh-bindal@users.noreply.github.com>
2024-05-28 14:39:42 +0530

    socket functionality fix

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server=app.listen(PORT,console.log(`Port is successfully created ${PORT}`)
 
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
-    cors: { origin: "http://localhost:3000"},
+    cors: { origin: "*"},
 
   });
   
--- client/src/components/SingleChat.js
+++ client/src/components/SingleChat.js
@@ -16,7 +16,7 @@ import Lottie from "react-lottie";
 import animationData from "../animations/typing.json"
 
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://connectique.onrender.com";
 var socket, selectedChatCompare;
 
 
